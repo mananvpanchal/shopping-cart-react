fd4cef908b45b6a7d06532be1b1bee4e9b1c57a4
Manan <mpanchal@integconsulting.com>
2018-05-05 18:39:46 +0530

    start serving by express

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const data = require('./data.js');
 const express = require('express')
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/list', (req, res) => res.send(data))
 
-app.listen(4000, () => console.log('Server listening on port 4000!'))
+app.listen(5000, () => console.log('Server listening on port 5000!'))
